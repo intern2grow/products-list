c14548c886955d47b7bab0d1c06dbc0db2093621
Mostafa Zewail77 <mostafazewail77@gmail.com>
2023-11-07 18:32:10 +0200

    Feature: Add quantity column with + and - buttons for product

--- src/App.js
+++ src/App.js
@@ -7,34 +7,61 @@ import Total from './components/total/total'
 class App extends Component {
   state = {
     items: [
-      {id:1, product:'Pen', price:2},
-      {id:2, product:'Book', price:10}
+      {id:1, product:'Pen', price:2, quantity:1},
+      {id:2, product:'Book', price:10, quantity:1}
     ]
   }
 
   deleteItem = (id) => {
-    let items = this.state.items
-    let i = items.findIndex(item => item.id === id)
-    items.splice(i, 1)
-    this.setState({items: items})
+    this.setState({
+      items: this.state.items.filter((item)=>item.id !== id )
+    })
   }
 
   addItem = (item) => {
     this.state.items.length > 0 ? (
       item.id = this.state.items[this.state.items.length - 1].id + 1 
     ) : item.id = 1
-    console.log(item.id)
+    // console.log(item.id)
     let items = this.state.items
     items.push(item)
     this.setState({items: items})
   }
+
+  increaseQuantity = (id)=>{
+    this.setState({
+      items: this.state.items.map((item)=>(
+        item.id === id ? {...item, quantity:item.quantity + 1}: item
+      ))
+    })
+  }
+
+  decreaseQuantity = (id)=>{
+    this.setState({
+      items: this.state.items.map((item)=>(
+        item.id === id && item.quantity > 0 
+        ? {...item, quantity:item.quantity - 1}: item
+      ))  
+    })
+    // After updating the quantity, check if it's zero and delete the item
+    const itemToDelete = this.state.items.find((item) => item.id === id);
+    if (itemToDelete && itemToDelete.quantity === 1) {
+      this.deleteItem(id);
+    }
+  }
+
 
   render() {
     return (
       <div className="container">
         <h1>Product List React App</h1>
         <div className="table">
-          <Items items={this.state.items} del={this.deleteItem}/>
+          <Items 
+            items = {this.state.items} 
+            del = {this.deleteItem}
+            increaseQuantity = {this.increaseQuantity}
+            decreaseQuantity= {this.decreaseQuantity}
+          />
           <AddItem add={this.addItem}/>
           <Total items={this.state.items}/>
         </div>
